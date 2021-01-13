c7f091bb632e0f04441a80235a0f0f8dc73731d9
No-Haiku <66711328+No-Haiku@users.noreply.github.com>
2021-01-13 17:21:00 -0300

    haiku store
    creacion de tienda

---
+++ project_tienda/ProyectowebApp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+
+def home(request):
+
+    return render(request,"ProyectowebApp/home.html")
+
+
+def tienda(request):
+
+    return render(request,"ProyectowebApp/tienda.html")
+
+
+
+def contacto(request):
+
+    return render(request,"ProyectowebApp/contacto.html")
