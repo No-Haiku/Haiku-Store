c78f9c77252f7779b863c6090bff3bfd4207e270
No-Haiku <66711328+No-Haiku@users.noreply.github.com>
2021-01-20 04:16:54 -0300

    contacto
    se agrego stylo ccs al formulario - add submit boton de enviar- csrf_token verificacion de envio de formulario

--- project_tienda/contacto/forms.py
+++ project_tienda/contacto/forms.py
@@ -5,7 +5,7 @@ class FormularioContacto(forms.Form):
     nombre= forms.CharField(label='Nombre', required=True)
     #apellido= forms.CharField(label='Apellido', required=True)
     email= forms.CharField(label='Email', required=True)
-    contenido= forms.CharField(label='Contenido', required=True)
+    contenido= forms.CharField(label='Contenido', required=True, widget=forms.Textarea)
 
 
     
