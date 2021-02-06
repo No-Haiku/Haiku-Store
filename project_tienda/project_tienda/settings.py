a13f1aa644b6a1598061f5029f95ba302f6c4fc0
No-Haiku <66711328+No-Haiku@users.noreply.github.com>
2021-02-05 23:35:29 -0300

    contacto en desarrollo

--- project_tienda/project_tienda/settings.py
+++ project_tienda/project_tienda/settings.py
@@ -127,3 +127,11 @@ STATIC_URL = '/static/'
 MEDIA_URL='/media/'
 
 MEDIA_ROOT=BASE_DIR / 'media'
+
+#enviar correo
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True  
+EMAIL_PORT=587
+EMAIL_HOST_USER="jhonkeee@gmail.com"
+EMAIL_HOST_PASSWORD="digoloquepienso"
